5e08be7270d803dc96d16fa06540513f4c859119
Osvald osmo <osmo@itu.dk>
2025-11-26 00:18:01 +0100

    Almost done

--- node/node.go
+++ node/node.go
@@ -1,9 +1,11 @@
 package proto
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"sync"
 	"time"
 
@@ -31,14 +33,14 @@ type Node struct {
 
 type AuctionState struct {
 	isOver    bool
-	Duration  int
+	Duration  int64
 	StartTime int64
-	topBid    int
+	topBid    int32
 	topBidder string
 	Bids      map[string]int
 }
 
-func newNode(id string, address string, peers []string, isLeader bool, Duration int) *Node {
+func newNode(id string, address string, peers []string, isLeader bool, Duration int64) *Node {
 	return &Node{
 		id:            id,
 		address:       address,
@@ -138,9 +140,170 @@ func (n *Node) sendHeartbeat() {
 }
 
 func (n *Node) checkHeartbeat() {
+	ticker := time.NewTicker(time.Second * 3)
+	for {
+		<-ticker.C
+		if n.isLeader {
+			continue
+		}
+		n.mu.RLock()
+		lastHB := n.lastHeartbeat[n.id]
+		n.mu.RUnlock()
+
+		currenttime := time.Now().Unix()
+		if currenttime-lastHB > 5 {
+			fmt.Printf("The leader is dead, long live the leader! ME: %s", n.id)
+			n.isLeader = true
+		}
+	}
+}
+
+func (n *Node) checkAuctionOver() {
+	ticker := time.NewTicker(time.Second)
+	for {
+		<-ticker.C
+		n.stateMutex.RLock()
+		currentTime := time.Now().Unix()
+		elapsedTime := currentTime - n.state.StartTime
+		if !n.state.isOver && elapsedTime >= n.state.Duration {
+			n.state.isOver = true
+			fmt.Printf("I (%s) declare this auction to be over!", n.id)
+			if n.isLeader {
+				go n.replicateToBackups()
+			}
+		}
+		n.stateMutex.RUnlock()
+	}
+}
+
+func (n *Node) replicateToBackups() {
+	n.stateMutex.RLock()
+
+	copyOfState := &proto.AuctionState{
+		Bidders:       make(map[string]bool),
+		Bids:          make(map[string]int32),
+		HighestBid:    n.state.topBid,
+		HighestBidder: n.state.topBidder,
+		StartTime:     n.state.StartTime,
+		Duration:      n.state.Duration,
+		IsOver:        n.state.isOver,
+	}
+	for bidder, amount := range n.state.Bids {
+		copyOfState.Bids[bidder] = int32(amount)
+		copyOfState.Bidders[bidder] = true
+	}
+	n.stateMutex.RUnlock()
+
+	n.clientMutex.RLock()
+	var clients []proto.AuctionServiceClient
+	for _, client := range n.peerClients {
+		clients = append(clients, client)
+	}
+	n.clientMutex.RUnlock()
+
+	for i := range clients {
+		go func(c proto.AuctionServiceClient) {
+			ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+			defer cancel()
+
+			req := &proto.ReplicateRequest{State: copyOfState}
+			_, err := c.Replicate(ctx, req)
+			if err != nil {
+				log.Printf("failed to replicate: %v", err)
+			}
+		}(clients[i])
+	}
+}
+
+func sendBid(nodeAddress string, bidder string, amount int32) (string, error) {
+	conn, err := grpc.Dial(nodeAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return "Exception", err
+	}
+	defer conn.Close()
+
+	client := proto.NewAuctionServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	req := &proto.BidRequest{
+		Bidder: bidder,
+		Amount: amount,
+	}
+
+	resp, err := client.Bid(ctx, req)
+	if err != nil {
+		return "Exception", err
+	}
+
+	return resp.Outcome, nil
+}
+
+func getResult(nodeAddress string) (string, error) {
+	conn, err := grpc.Dial(nodeAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return "Exception", err
+	}
+	defer conn.Close()
+
+	client := proto.NewAuctionServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	req := &proto.ResultRequest{}
+
+	resp, err := client.Result(ctx, req)
+	if err != nil {
+		return "Exception", err
+	}
+
+	return resp.Message, nil
 }
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run node.go client bid <node-address> <bidder> <amount>")
+		fmt.Println("Or go run node.go client result <node-address>")
+		return
+	}
+
+	if os.Args[1] == "client" {
+		if len(os.Args) < 4 {
+			fmt.Println("Invalid command, reason: length")
+			return
+		}
+		command := os.Args[2]
+		if command == "bid" {
+			if len(os.Args) != 6 {
+				fmt.Println("Invalid command, reason: length")
+				return
+			}
+			nodeAddress := os.Args[3]
+			bidder := os.Args[4]
+			var amount int32
+			fmt.Sscanf(os.Args[5], "%d", &amount)
+			result, err := sendBid(nodeAddress, bidder, amount)
+			if err != nil {
+				fmt.Println("Error sending bid:", err)
+				return
+			}
+			fmt.Println("Bids sent:", result)
+		} else if command == "result" {
+			if len(os.Args) != 4 {
+				fmt.Println("Invalid command, reason: length")
+				return
+			}
+			nodeAddress := os.Args[3]
+
+			result, err := getResult(nodeAddress)
+			if err != nil {
+				fmt.Println("Error getting result:", err)
+			}
+			fmt.Println(result)
+		}
+		return
+	}
+
 	var peers []string
 	var node1 = newNode("Node1", "localhost:5001", peers, true, 100)
 	var node2 = newNode("Node2", "localhost:5002", peers, false, 100)
@@ -160,5 +323,9 @@ func main() {
 	peers3[0] = node1.address
 	peers3[1] = node2.address
 	node3.peers = peers3
+	node1.startNode()
+	node2.startNode()
+	node3.startNode()
 
+	select {}
 }
